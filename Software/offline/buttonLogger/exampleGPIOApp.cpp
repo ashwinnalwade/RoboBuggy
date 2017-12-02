799ce7aab19fa5c9f638bc61dbaf57a669451d29
StanleyTX2 <Stanley@TX2>
2017-12-02 19:51:55 +0000

    (vivaan) committing to do other work

--- Software/offline/buttonLogger/exampleGPIOApp.cpp
+++ Software/offline/buttonLogger/exampleGPIOApp.cpp
@@ -10,7 +10,22 @@
 #include <string>
 #include <unistd.h>
 #include "jetsonGPIO.h"
+#include <sstream>
+
 using namespace std;
+
+std::string exec(char* cmd) {
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) return "ERROR";
+    char buffer[128];
+    std::string result = "";
+    while(!feof(pipe)) {
+        if(fgets(buffer, 128, pipe) != NULL)
+            result += buffer;
+    }
+    pclose(pipe);
+    return result;
+}
 
 int getkey() {
     int character;
@@ -38,29 +53,46 @@ int getkey() {
 int main(int argc, char *argv[]){
 
 //    cout << "Starting Logs" << endl;
+    if (argc != 3) {
+        cout << ("Need 2 arguments: date and day") << endl;
+	return 1;
+    }
 
-    int redLED = 398;     // Ouput */
+    std::string date;
+    std::string day;
+    date = argv[1];
+    day = argv[2];
+
+    std::ostringstream oss;
+
+    int counter = 1;
+
+    int greenLED = 398; // LED to acknowledge button press
+    int roscoreLED = 298; // LED to signify roscore is running
+    int rosbagLED = 388; // LED to signify rosbag is running
+
+    char*  rosbagchk = "if ps -e | grep \"rosbag\" > \\dev\\null \n then echo 1 \n else echo 0 \n fi";
+    char*  roscorechk = "if ps -e | grep \"roslaunch\" > \\dev\\null \n then echo 1 \n else echo 0 \n fi";
+    cout<<atoi(exec(rosbagchk).c_str())<<endl;
+
     int pushButton = 481; // Input
-    // Make the button and led available in user space
-    gpioUnexport(redLED);
+    // Make the button and LEDs available in user space
+    gpioUnexport(greenLED);
+    gpioUnexport(roscoreLED);
+    gpioUnexport(rosbagLED);
+
     gpioExport(pushButton);
-    gpioExport(redLED); 
-    gpioSetDirection(pushButton,inputPin) ;
-    gpioSetDirection(redLED,outputPin) ;
+    gpioExport(greenLED);
+    gpioExport(roscoreLED);
+    gpioExport(rosbagLED);
+
+    gpioSetDirection(pushButton, inputPin);
+    gpioSetDirection(greenLED, outputPin);
+    gpioSetDirection(roscoreLED, outputPin);
+    gpioSetDirection(rosbagLED, outputPin);
     // Reverse the button wiring; this is for when the button is wired
     // with a pull up resistor
     // gpioActiveLow(pushButton, true);
-
-
-    // Flash the LED 5 times
-    /*for(int i=0; i<5; i++){
-        cout << "Setting the LED on" << endl;
-        gpioSetValue(redLED, on);
-        usleep(200000);         // on for 200ms
-        cout << "Setting the LED off" << endl;
-        gpioSetValue(redLED, off);
-        usleep(200000);         // off for 200ms
-    }*/
 
     // Wait for the push button to be pressed
     cout << "Please press the button! ESC key quits the program" << endl;
@@ -69,36 +101,46 @@ int main(int argc, char *argv[]){
     //    ledValue = low ; 
     bool running = false;
     // Turn off the LED
-    gpioSetValue(redLED,low) ; 
+    gpioSetValue(greenLED,low) ; 
     while(getkey() != 27) {
         gpioGetValue(pushButton, &value) ;
         // Useful for debugging
         //cout << "Button " << value << endl;
         if (value==high) {
+            gpioSetValue(greenLED, on);
             if (running) {
-                system("./script_stop.sh");
-		cout<<"Logs STOPPED"<<endl;
+                system("./script_stop.sh ");
+        		cout<<"Logs STOPPED"<<endl;
                 running = false;
-		gpioSetValue(redLED, off);
-		while(value==high) {
-			gpioGetValue(pushButton, &value) ;
-		}
+        		while(value==high) {
+		        	gpioGetValue(pushButton, &value) ;
+		        }
             } else {
-                system("./script_run.sh");
+                oss << "./script_run.sh " << date << " " << day << " " << counter;
+                system(oss.str().c_str());
                 cout<<"Logs STARTED" <<endl;
                 running = true;
-		gpioSetValue(redLED, on);
-		while(value==high) {
-			gpioGetValue(pushButton, &value) ;
-		}
+                counter += 1;
+		        while(value==high) {
+			        gpioGetValue(pushButton, &value) ;
+        		}
             }
-            // button is pressed ; turn the LED on
+        } else {
+            gpioSetValue(greenLED, off);
         }
+	if(atoi(exec(roscorechk).c_str()))
+            gpioSetValue(roscoreLED, on);
+        else
+            gpioSetValue(roscoreLED, off);
+	if(atoi(exec(rosbagchk).c_str()))
+            gpioSetValue(rosbagLED, on);
+        else
+            gpioSetValue(rosbagLED, off);
         usleep(1000); // sleep for a millisecond
     }
 
     cout << "Logging Finished" << endl;
-    gpioUnexport(redLED);     // unexport the LED */
+    gpioUnexport(greenLED);     // unexport the LED */
     gpioUnexport(pushButton);      // unexport the push button
     return 0;
 }
--- Software/offline/buttonLogger/script_run.sh
+++ Software/offline/buttonLogger/script_run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
+ls
 cd ~/RoboBuggy/Software/real_time/ROS_RoboBuggy/
 source devel/setup.bash
-ls
-cd ../../
 roslaunch robobuggy transistor.launch &
-rosbag record -a &
+
+cd ~/RoboBuggy/Software/logs/$1
+
+i=0
+arr1=($(for f in *.bag; do n=${f%.*}; num=${n: -1}; array[$i]=$num; i=$((i+1)); echo $num; done | sort))
+echo $((${arr1[-1]}+1))
+
+rosbag record -a -O $2_fr$((${arr1[-1]}+1)) &
